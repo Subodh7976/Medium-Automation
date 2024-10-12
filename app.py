fc24a89679c5adb2b4bb23564cfe84709057fc4c
Subodh7300 <97154559+Subodh7300@users.noreply.github.com>
2024-10-12 17:32:07 +0530

    Added initial project

---
+++ app.py
@@ -0,0 +1,5 @@
+from fastapi import FastAPI
+from fastapi.responses import StreamingResponse, HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from dotenv import load_dotenv
+load_dotenv()
